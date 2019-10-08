e5ff8fec149d32288b4e5978b6411b9e6636a9fa
Dane Fang <daniel.fangtz@gmail.com>
2019-10-08 14:40:55 -0600

    first commit

---
+++ BLE_A2.Android/obj/Debug/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 3e8b1ca4a44958b0fcf44d3dc714931a98c4da87 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1454
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 380949
+	.include	"typemap.mj.inc"
---
+++ BLE_A2/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using Acr.Collections;
+using Acr.UserDialogs;
+using Plugin.BluetoothLE;
+using Prism.Navigation;
+using ReactiveUI;
+
+namespace BLE_A2
+{
+    public class MainViewModel : ViewModel
+    {
+    }
+}
